25b78df682ea8f8c3065468cd291cf6c7ce0ebe7
Sean Balbale <sean.balbale@gmail.com>
2024-03-04 15:16:33 +0000

    ran program

---
+++ Code/C++Assignment01/cmake-build-debug/CMakeFiles/C__Assignment01.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__Assignment01.dir/main.cpp.obj"
+  "CMakeFiles/C__Assignment01.dir/main.cpp.obj.d"
+  "C__Assignment01.exe"
+  "C__Assignment01.exe.manifest"
+  "C__Assignment01.pdb"
+  "libC__Assignment01.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__Assignment01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Code/C++Assignment01/cmake-build-debug/cmake_install.cmake
+++ Code/C++Assignment01/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/seanb/OneDrive/Desktop/Extra/Coding/C++/Cornerstone2/C++Assignment01
+# Install script for directory: C:/Users/seanb/Documents/Northeastern-GE1501-Cornerstone-of-Engineering-2/Code/C++Assignment01
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.2.1/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Program Files/JetBrains/CLion 2023.3.4/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/seanb/OneDrive/Desktop/Extra/Coding/C++/Cornerstone2/C++Assignment01/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/seanb/Documents/Northeastern-GE1501-Cornerstone-of-Engineering-2/Code/C++Assignment01/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
