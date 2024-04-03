bdff8fcc72607d248319ea0d1faeca82a386b6d2
Plumagednine <47873648+Plumagednine@users.noreply.github.com>
2024-04-03 16:11:21 -0400

    code

--- Code/MatLab/matlab assignment 2/part1.m
+++ Code/MatLab/matlab assignment 2/part1.m
@@ -15,6 +15,13 @@ eq1 = 4*a - 2*b + 6*d == 12 + 3*c;
 eq2 = 7*b + 6.5*c - 6*d + 6.5 == 6*a;
 eq3 = a - 16 + 7.5*b + 6.25*c + 5.5*d == 0;
 eq4 = -d + 22*b - 12*a + 15.5*c == 17;
-[A, B] = equationsToMatrix([eq1, eq2, eq3, eq4], [a, b, c, d]);
-X = linsolve(A, B);
-disp(X);
+
+sol = solve([eq1, eq2, eq3, eq4], [a, b, c, d]);
+aSol = sol.a;
+bSol = sol.b;
+cSol = sol.c;
+dSol = sol.d;
+
+disp(sol);
+
+
---
+++ Code/MatLab/matlab assignment 2/part2.m
@@ -0,0 +1,25 @@
+% Write an mfile and in that:
+% Create a 1D array (row) for x from -2 to 0 with increment of 0.2
+% Create another 1D array (row) for y from -1 to 1 with increment of 0.2
+% Calculate this function for this range of x and y:
+% z=xe(-(sin(x))^2-(sin(y))^2)
+% write the values of function z for this range of x and y. For that you need to use fprinf as below:
+% fprintf('for %4.2f <x< %4.2f and %4.2f<y<%4.2f we have %4.2f <z< %4.2f \n',x(1),x(end),y(1),y(end),z(1),z(end));
+% Now set sin(y)=1 so we will have this new function:
+% z_two=xe(-(sin(x))^2-1^2)  
+% plot function z_two for the range of x you created above. Include a title (any title you like) and label for your axes and put the line color as black.
+
+x = -2:0.2:0;
+y = -1:0.2:1;
+z = x.*exp(-(sin(x)).^2-(sin(y)).^2);
+
+fprintf('for %4.2f <x< %4.2f and %4.2f<y<%4.2f we have %4.2f <z< %4.2f \n',x(1),x(end),y(1),y(end),z(1),z(end));
+
+z_two = x.*exp(-(sin(x)).^2-1^2);
+
+figure
+plot(x,z_two,'k')
+title('Plot of z=xe^(-(sin(x))^2-1^2)')
+xlabel('x')
+ylabel('z')
+grid on
